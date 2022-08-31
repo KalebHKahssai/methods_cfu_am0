249bca4abee46a16ff28604ef5730f6d9a9295cc
Kaleb Kahssai <kalebkahssai@gmail.com>
2022-08-30 20:43:51 -0700

    Final update of Methods CFU

--- final_practice.rb
+++ final_practice.rb
@@ -1,18 +1,20 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
 
+# method definition
 def greeting
   puts "Yo"
 end
 
+# method call
+greeting
 
- # greeting call
+# greeting call
 
 # What is the return value of your method?
   # This would "nil" because of the puts command.
 # How many arguments did you pass your method?
   # This one has no arguments.
-
 
 
 
@@ -22,11 +24,8 @@ def custom_greeting(name)
   puts "Yo, #{name}"
 end
 
- puts custom_greeting ("Kaleb")
+custom_greeting("Kaleb")
 
-
-
-#
 
 # What is the return value of your method?
   # This would "nil" because of the puts command.
@@ -44,12 +43,14 @@ def square(num1)
   num1 * num1
 end
 
-p
+square(2)
 
 # What is the return value of your method?
+  # 4
 # How many arguments did you pass your method?
     # This one has one arguemnt.
 # What data type was your argument(s)?
+  #An integer
 
 
 
@@ -57,14 +58,14 @@ p
 #4: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
 
 def greet_person(first_name, middle_name, last_name)
-  puts "Yo, #{first_name} #{middle_name} #{last_name}"
+   "Yo, #{first_name} #{middle_name} #{last_name}"
 end
 
- puts greet_person ("Kaleb", "Haftu", "Kahssai")
+ puts greet_person("Kaleb", "Haftu", "Kahssai")
 
 # What is the return value of your method?
   # This would "nil" because of the puts command.
 # How many arguments did you pass your method?
   # There are three arguments.
 # What data type was your argument(s)?
-  #
+  # String object 
--- self_eval.md
+++ self_eval.md
@@ -1,5 +1,8 @@
 # Self Evaluation
 
 - How confident do you feel about your use of built-in Ruby methods?
+  * I feel fairly confident in my use of built-in Ruby methods. I feel like it's straightforward, just need more reps to feel fully confident.
 - How confident do you feel writing simple methods of your own?
+  * On a scale of 1 to 5, I feel that I am about a 3 just because conceptually it hasn't fully clicked. I plan to do some independent research.
 - What questions do you have about methods, and what they are capable of? (ask a question!!!)
+  * I need a further explanation on "!".
